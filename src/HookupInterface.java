27a75cac25a6a86e58755ffd580e827a320f76ef
komalb711 <komalbhavsar711@gmail.com>
2019-01-31 15:29:07 -0500

    Initial hookup interface, initial debugger

---
+++ src/HookupInterface.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public interface HookupInterface{
+
+    public boolean checkCandidateList(List<Integer> candidateList, int queryNodeId);
+
+//    public boolean check();
+
+
+
+}
--- src/Interface.java
+++
@@ -1,5 +0,0 @@
-public class Interface {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
